725df3b2558ecb085a9399f843b6d904562120f5
Rick Mark <rickmark@dropbox.com>
2018-11-14 15:14:18 -0800

    Initial Commit

---
+++ certificate-sync/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  certificate-sync
+//
+//  Created by Rick Mark on 11/14/18.
+//  Copyright © 2018 Dropbox. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
